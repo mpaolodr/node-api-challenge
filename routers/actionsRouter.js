8ad7b28777ab4e1e851078cd118d6c3486ed9050
mpaolodr <mpaolodr_1504@yahoo.com>
2020-02-14 12:23:18 -0500

    put request for actions completed

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -4,6 +4,7 @@ const Act = require("../data/helpers/actionModel.js");
 
 // CUSTOM MIDDLEWARE
 const validateActionId = require("../api/middlewares/actionsMiddleware.js");
+const { validateAction } = require("../api/middlewares/projectsMiddleware.js");
 
 router.get("/", (req, res) => {
   Act.get()
@@ -21,6 +22,37 @@ router.get("/:id", validateActionId, (req, res) => {
   Act.get(id)
     .then(action => {
       res.status(200).json(action);
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
+
+router.delete("/:id", validateActionId, (req, res) => {
+  const { id } = req.params;
+
+  Act.get(id)
+    .then(toBeDeleted => {
+      Act.remove(id)
+        .then(deleted => {
+          res.status(200).json(toBeDeleted);
+        })
+        .catch(err => {
+          res.status(500).json({ errorMessage: err.message });
+        });
+    })
+    .catch(err => {
+      res.status(500).json({ errorMessage: err.message });
+    });
+});
+
+router.put("/:id", validateActionId, validateAction, (req, res) => {
+  const { id } = req.params;
+  const updatedAction = req.body;
+
+  Act.update(id, updatedAction)
+    .then(updated => {
+      res.status(200).json(updated);
     })
     .catch(err => {
       res.status(500).json({ errorMessage: err.message });
